ab42d302914ec9bf63157f47b297d5af66d240c0
Elkin E. Hernandez Fajardo <ehernandez04@its1.local>
2022-08-24 18:22:12 -0500

    Se completa el modelamiento de la data para poder ser procesada en POWER BI

--- Disponibilidad_Red_UTIL.py
+++ Disponibilidad_Red_UTIL.py
@@ -1,23 +1,32 @@
+
 import pandas as pd
 import numpy as np
 import datetime
 
 pd.options.mode.chained_assignment = None  # default='warn'
-nombreArchivo= "2022-08-02_21-23-17.csv"
-directorio=  "../2. DATAS/2.2 DATAS HISTORICAS/" + nombreArchivo
+nombreArchivo= '2022-08-02_21-23-17.csv'
+nombreDocNodos= 'NodosR2V_TRBONET.xlsx'
+directorio=  '../2. DATAS/2.2 DATAS HISTORICAS/' + nombreArchivo
+directorioNodos= '../2. DATAS/2.2 DATAS HISTORICAS/' + nombreDocNodos
 
 
-def leer_archivo():
-    #lee el archivo en el directorio dado 
-    df = pd.read_csv(directorio , sep="," , encoding='latin-1')
-    return df
+def leer_archivo(nombreArchivo , tipo):
+    if tipo=='csv':
+        #lee el archivo en el directorio dado 
+        df = pd.read_csv(nombreArchivo , sep=',' , encoding='latin-1')
+        return df
+    elif tipo== 'xlsx':
+        #lee el archivo en el directorio dado 
+        df = pd.read_excel(nombreArchivo)
+        return df
+        
 
 def generarNombresDeNodos(df):
     df2= pd.DataFrame()
     df2= df['Managed Resource'].copy()
     
     #Reemplaza todas las Castilla # por solo Castilla
-    df2= df2.replace(["Castilla 1", "Castilla 2", "Castilla 3" , "Castilla 4"], "Castilla")
+    df2= df2.replace(['Castilla 1', 'Castilla 2', 'Castilla 3' , 'Castilla 4'], 'Castilla')
     
     #Separa en 2 columnas para dejar solo el nombre del nodo y en otra columna el número del repetidor
     df2= df2.str.split('RPT', expand=True)
@@ -65,62 +74,193 @@ def GenerarConteoDeReps(nodos , df):
 
 def generarRelacionesFechas(df, n_nodos):
 
-    df.insert(8,"Final Falla", np.nan)
+    df.insert(8,'Final Falla Primer Clear', np.nan)
     #Para realizar prueba final
     for fila in range(len(nombresNodos)):
-        #dfPrueba=pd.DataFrame()
+        #obtiene el nombre de todos los repetidores por nodo y los guarda en un arreglo
         auxRep=n_nodos.loc[fila,'Nombre Repetidores']
+        
+        #Variables que funcionan como testigos para poder realizar la operación
+        indexFailure= 'vacio'
+        horaClear= 'vacio'
 
-        indexFailure= "vacio"
-        horaClear= "vacio"
-
+        #Empieza a evaluar cada nombre de cada repetidor por nodo
         for NodoRep in auxRep:
+            #Se genera un df auxiliar para ejecutar el filtro del repetidor en evaluación
             dfaux=pd.DataFrame()
             dfaux= pd.concat([dfaux , df[df['Managed Resource']==NodoRep] ])
             dfaux.reset_index(inplace=True)
             
-            variasHorasClear=[]
+            #Lista para almacenar todos los clear después de que hay un CommFailure
+            #variasHorasClear=[]
+
+            #Obtener la longitud del df filtrado
             longitud=len(dfaux)
 
             for i in range(longitud):
-                
-                if dfaux['Severity'].iloc[i]== "CommFailure":
-                    if len(variasHorasClear) >=1:        
-                        dfaux.at[indexFailure, 'Final Falla'] = variasHorasClear
-                        variasHorasClear=[]
-                        indexFailure="vacio"
-                        horaClear="vacio"
-
+                #si hay un CommFailure haga lo siguiente
+                if dfaux['Severity'].iloc[i]== 'CommFailure':
+                    #si ya han habido registros de fechas para guardarlas antes de modificar el indexFailure
+                    #if len(variasHorasClear) >=1:        
+                    #    dfaux.at[indexFailure, 'Final Falla Primer Clear'] = variasHorasClear[0] #TOMA UNICAMENTE LA PRIMERA FECHA DADAS INDICACIONES DE ADRIANO
+                    #    variasHorasClear=[]
+                    #    indexFailure='vacio'
+                    #    horaClear='vacio'
+                    #Si indexFailure está definido como "vacio"
                     indexFailure=dfaux.index[i]
-                    
-                elif dfaux['Severity'].iloc[i]== "Clear" and indexFailure!="vacio":
+                    horaClear='vacio'
+                #Si hay un Clear y anteriormente ha habido un CommFailure    
+                elif dfaux['Severity'].iloc[i]== 'Clear' and indexFailure!='vacio' and horaClear=='vacio':
                     horaClear=dfaux['Date/Time'].iloc[i]
-                    variasHorasClear.append(horaClear.strftime("%Y-%m-%d %H:%M:%S"))
-                    
-                
-                if i== (longitud-1) and len(variasHorasClear)>=1 and indexFailure!="vacio":
-                    dfaux.at[indexFailure, 'Final Falla'] = variasHorasClear
-                    variasHorasClear=[]
-                    indexFailure="vacio"
-                    horaClear="vacio"
-            
+                    dfaux.at[indexFailure, 'Final Falla Primer Clear'] = horaClear
+                    #Guarda en el arreglo la hora en string
+                    #variasHorasClear.append(horaClear.strftime('%Y-%m-%d %H:%M:%S'))
+                    #variasHorasClear.append(horaClear)
+
+                #Si es el ultimo registro y el arreglo de horas está lleno y la variable indexFailure tiene registrado un indice
+                #if i== (longitud-1) and len(variasHorasClear)>=1 and indexFailure!='vacio':
+                #    dfaux.at[indexFailure, 'Final Falla Primer Clear'] = variasHorasClear[0] #TOMA UNICAMENTE LA PRIMERA FECHA DADAS INDICACIONES DE ADRIANO
+                #    variasHorasClear=[]
+                #    indexFailure='vacio'
+                #    horaClear='vacio'
+            #Se pasa del df_auxiliar a la data original
             for cic in range(len(dfaux)):
                 indicedata= dfaux['index'].iloc[cic]
-                horaFinalFalla= dfaux['Final Falla'].iloc[cic]
-                df.at[indicedata, 'Final Falla'] = horaFinalFalla
+                horaFinalFalla= dfaux['Final Falla Primer Clear'].iloc[cic]
+                df.at[indicedata, 'Final Falla Primer Clear'] = horaFinalFalla
+    #Elimina los Clear y los que no tienen hora final de falla
+    df = data.dropna(subset=['Final Falla Primer Clear'])
+    
+    #Comprobación para ver todas las alertas incluso las que no tienen hora final de falla
+    #df= data[data['Severity']=='CommFailure']
+    
+    df.reset_index(drop=True , inplace=True)
+    df['Final Falla Primer Clear']= pd.to_datetime(df['Final Falla Primer Clear'])  
     return df
 
-data= leer_archivo()
+def indispoSegMin(df):
+    df['Segundos Indispo']= (df['Final Falla Primer Clear'] - df['Date/Time']).dt.seconds
+    df['Minutos Indispo']= df['Segundos Indispo']/60
+    return df
+
+def AjusteVariosDias(data):
+    #Obtiene los indices donde se pasa del dia de inicio de falla 
+    ind = list(np.where(data['Date/Time'].dt.day != data['Final Falla Primer Clear'].dt.day))
+
+    for indexData in ind[0]:
+        #Obtiene variables
+        diaInicialAux= int(data['Date/Time'].iloc[indexData].strftime('%d'))
+        diaFinalAux=int(data['Final Falla Primer Clear'].iloc[indexData].strftime('%d'))
+        mes=int(data['Final Falla Primer Clear'].iloc[indexData].strftime('%m'))
+        anio=int(data['Final Falla Primer Clear'].iloc[indexData].strftime('%Y'))
+        
+        filaIndexar= data.iloc[indexData].to_dict()
+        
+        for dia in range(diaInicialAux , diaFinalAux+1):
+            #Este será el dia inicial
+            if dia == diaInicialAux:
+                #Pone la fecha final falla hasta la ultima hora del dia de la fecha del comFailure
+                fechAux= pd.to_datetime( str(diaInicialAux) + str(mes) +str(anio) + ' ' + '23:59:59' , format='%d%m%Y %H:%M:%S' )
+                data.at[indexData, 'Final Falla Primer Clear'] = pd.to_datetime(fechAux, format='%d%m%Y %H:%M:%S' )
+            elif dia != diaInicialAux and dia!= diaFinalAux:
+                #Para días intermedios
+                filaAux= filaIndexar.copy()
+                fechIniAux= pd.to_datetime( str(dia) + str(mes) +str(anio) + ' ' + '00:00:00' , format='%d%m%Y %H:%M:%S' )
+                fechFinAux= pd.to_datetime( str(dia) + str(mes) +str(anio) + ' ' + '23:59:59' , format='%d%m%Y %H:%M:%S' )
+                filaAux['Date/Time'] = fechIniAux
+                filaAux['Final Falla Primer Clear']= fechFinAux
+
+                data.loc[len(data)]=filaAux
+                
+            elif dia == diaFinalAux:
+                #Para el último día , o día en que la falla se terminó
+                filaAux= filaIndexar.copy()
+                fechIniAux= pd.to_datetime( str(diaFinalAux) + str(mes) +str(anio) + ' ' + '00:00:00' , format='%d%m%Y %H:%M:%S' )
+                filaAux['Date/Time'] = fechIniAux
+                data.loc[len(data)]=filaAux
+    return data
+
+def Condicion_Mayor_5_Min(df):
+    df["Int Mayor a 5 Min"] = "NO" 
+    ind = list(np.where(df['Segundos Indispo'] >= 300))
+    indice= ind[0]
+    for j in indice:
+        df['Int Mayor a 5 Min'].iloc[j] = "SI"
+    return df
+
+def Condicion_Degradacion_Servicio(df , nodosReps):
+    for fila in range(len(nodosReps)):
+        longAux= len(nodosReps['Nombre Repetidores'].iloc[fila])
+        #print(longAux)
+        #print( nodosReps['Nombre Repetidores'].iloc[fila])
+        
+        for repetidor in nodosReps['Nombre Repetidores'].iloc[fila]:
+            #Se genera un df auxiliar para filtrar por el repetidor en evaluación
+            dfaux=pd.DataFrame()
+            dfaux= pd.concat([dfaux , df[df['Managed Resource']==repetidor] ])
+            dfaux.reset_index(inplace=True)
+            dfaux= dfaux.sort_values(by='Date/Time') 
+            #Obtener la longitud del df filtrado
+            longitud=len(dfaux)
+            
+            dfaux['Lapso Entre Fallas']=0
+            dfaux['Aplica Degradacion Serv']="NO"
+            for i in range( 1 , longitud ):
+                
+                final= dfaux['Final Falla Primer Clear'].iloc[i-1]
+                inicial= dfaux['Date/Time'].iloc[i]
+                
+                dif=(inicial - final).total_seconds()
+                dfaux['Lapso Entre Fallas'].iloc[i]= dif
+
+                if dif <= 3600:
+                    dfaux['Aplica Degradacion Serv'].iloc[i-1]='SI'
+                    dfaux['Aplica Degradacion Serv'].iloc[i]='SI'
+
+
+            #Se pasa del df_auxiliar a la data original
+            for cic in range(len(dfaux)):
+                indicedata= dfaux['index'].iloc[cic]
+                lapso= dfaux['Lapso Entre Fallas'].iloc[cic]
+                aplica= dfaux['Aplica Degradacion Serv'].iloc[cic]
+                df.at[indicedata, 'Lapso Entre Fallas'] = lapso
+                df.at[indicedata, 'Aplica Degradacion Serv'] = aplica        
+    return df
+
+def relacionCantNodosTotales(df_final, dataOficialNodos):
+    df_final['Cant Nodos']= np.nan
+    for i in range(len(dataOficialNodos)):
+        ind = list(np.where(df_final['Nombre Nodo'] == dataOficialNodos['Nombre Nodo'].iloc[i]))
+        indice= ind[0]
+        for j in indice:
+            df_final['Cant Nodos'].iloc[j] = dataOficialNodos['Cantidad de Repetidores'].iloc[i]
+    
+    return df_final
+
+def quitarSegundosFechas(data):
+    data["Disponibilidad"]= 1 - (data['Segundos Indispo'] / 86400)
+    data['Date/Time Sin Seg'] = data['Date/Time'].dt.strftime('%d/%m/%Y %H:%M')
+    data['Final Falla Primer Clear Sin Seg'] = data['Final Falla Primer Clear'].dt.strftime('%d/%m/%Y %H:%M')
+    data['AplicaIndicador']= np.where(((data['Aplica Degradacion Serv']=='SI') | (data['Int Mayor a 5 Min']=='SI')),'SI','NO')
+    return data
+
+
+data= leer_archivo(directorio , 'csv')
+dataNodos= leer_archivo(directorioNodos , 'xlsx')
 data= formatearFecha(data)
 nombresNodos= generarNombresDeNodos(data)
 data= insertarNombresNodos(data , nombresNodos)
-'''DATA'''
-data
 nombresNodos= GenerarConteoDeReps(nombresNodos , data)
 data= generarRelacionesFechas(data, nombresNodos)
-
+data= AjusteVariosDias(data)
+data= indispoSegMin(data)
+data= Condicion_Mayor_5_Min(data)
+data= Condicion_Degradacion_Servicio(data, nombresNodos)
+data= relacionCantNodosTotales(data , dataNodos)
+data=quitarSegundosFechas(data)
 '''TABLA CON NOMBRES DE NODOS , CANTIDAD DE REPETIDORES Y NOMBRES DE REPETIDORES'''
-nombresNodos
+#nombresNodos
+
 '''DATA CON VALORES DE TIEMPO FINAL DE FALLA O CLEAR'''
-data
-#data.to_excel("PruebaFinal.xlsx")
+#data
+data.to_excel("PruebaDataTRBONET.xlsx")
